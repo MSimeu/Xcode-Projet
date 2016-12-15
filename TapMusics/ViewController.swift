92b0063a62d1741252174844de0ba410af252f90
Menes SIMEU <menes.simeu@gmail.com>
2016-12-15 12:13:51 +0100

    Initial Commit

---
+++ TapMusics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TapMusics
+//
+//  Created by MENES SIMEU on 15/12/2016.
+//  Copyright © 2016 MenesS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
